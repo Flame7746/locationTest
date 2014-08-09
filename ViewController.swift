c541efa6e39ecc1eea59b9ad29c2de06e9ef439e
MichaelSenoyuit <msenoyui@catherines-mbp.home>
2014-08-08 23:43:39 -0400

    main

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  locationTest
+//
+//  Created by Michael Senoyuit IV on 8/8/14.
+//  Copyright (c) 2014 EGO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
